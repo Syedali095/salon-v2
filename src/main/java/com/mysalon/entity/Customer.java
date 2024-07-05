d015c0e40a060343291c65079ebe50fdc3431312
Syedali095 <syedmohsin095@gmail.com>
2024-07-05 20:41:53 +0530

    Fifth commit - Clone done

--- src/main/java/com/mysalon/entity/Customer.java
+++ src/main/java/com/mysalon/entity/Customer.java
@@ -11,7 +11,6 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
